8a9e457328989c7edbb43e79bd4b48495f13c43d
jarelychez <jarelychez@.com>
2025-08-20 09:27:03 -0400

    uploading customer-api

---
+++ Backend/customer-api/src/main/java/com/example/customer_api/repository/CustomerRepository.java
@@ -0,0 +1,9 @@
+package com.example.customer_api.repository;
+
+import com.example.customer_api.model.Customer;
+import org.springframework.data.jpa.repository.JpaRepository;
+import java.util.Optional;
+
+public interface CustomerRepository extends JpaRepository<Customer, Long> {
+    Optional<Customer> findByName(String name);
+}
